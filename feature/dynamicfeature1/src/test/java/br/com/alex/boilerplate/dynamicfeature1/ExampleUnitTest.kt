57546ab5f488a0111d32ffa982c26f5a6197591a
Alex Almeida Tavella <alexkotr@gmail.com>
2019-11-09 14:55:40 -0200

    Adding more modules and CI configuration

---
+++ feature/dynamicfeature1/src/test/java/br/com/alex/boilerplate/dynamicfeature1/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package br.com.alex.boilerplate.dynamicfeature1
+
+import io.kotlintest.shouldBe
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        2 + 2 shouldBe 4
+    }
+}
